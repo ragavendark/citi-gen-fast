6954a2c36a0ab14fcc308218628987023fec6df7
ragavendar <krishnaharish678@gmail.com>
2026-03-20 09:17:49 +0000

    crud operations using service and controller

--- api/v1/routes/users.py
+++ api/v1/routes/users.py
@@ -1,8 +1,35 @@
 from fastapi import APIRouter
+from api.v1.services.user_services import UserService
 
 router = APIRouter(prefix="/users", tags=["users"])
+
+user_service = UserService()
 
 
 @router.get("/")
 def get_users():
-    return [{"id": 1, "name": "Jack Sparrow"}, {"id": 2, "name": "Hector Barbosa"}]
+    return user_service.get_users()
+
+
+# url: loclahost:8000/api/v1/users -- POST
+@router.post("/")
+def create_user(user: dict):
+    return user_service.create_user(user)
+
+
+# url: loclahost:8000/api/v1/users/1 or /2 --GET
+@router.get("/{user_id}")
+def get_user_by_id(user_id: int):
+    return user_service.get_user_by_id(user_id)
+
+
+# url: loclahost:8000/api/v1/users/1 or /2 --PUT
+@router.put("/{user_id}")
+def update_user_by_id(user_id: int, user: dict):
+    return user_service.update_user_by_id(user_id, user)
+
+
+# url: loclahost:8000/api/v1/users/1 or /2 --DELETE
+@router.delete("/{user_id}")
+def get_user_by_id(user_id: int):
+    return user_service.delete_user_by_id(user_id)
---
+++ api/v1/services/user_services.py
@@ -0,0 +1,34 @@
+class UserService:
+    def __init__(self):
+        self.users_list = [
+            {"id": 1, "name": "Jack Sparrow"},
+            {"id": 2, "name": "Hector Barbosa"},
+        ]
+
+    def get_users(self):
+        return self.users_list
+
+    def create_user(self, user: dict):
+        print(f"Recieved {user}")
+        self.users_list.append(user)
+        return {"status": " post success"}
+
+    def get_user_by_id(self, user_id: int):
+        print(f"Received user id: {user_id}")
+        for i in self.users_list:
+            if i["id"] == user_id:
+                return i
+
+    def update_user_by_id(self, user_id: int, user: dict):
+        print(f"Received user id: {user_id}")
+        for index in range(len(self.users_list)):
+            if self.users_list[index]["id"] == user_id:
+                self.users_list[index] = user
+                return {"status": "update success"}
+
+    def delete_user_by_id(self, user_id: int):
+        print(f"Received user id: {user_id}")
+        for index in range(len(self.users_list)):
+            if self.users_list[index]["id"] == user_id:
+                self.users_list.pop(index)
+                return {"status": "deletion  success"}
